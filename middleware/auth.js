2c17c0fc64c4622cbab1b011f21cf2b5872ab7a7
Rafe Ibrahim <rafe.ibrahim00@gmail.com>
2019-12-03 11:07:15 +0200

    userRoutes --> CRUD + login  done + authentication middleware inplace using jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,38 @@
 'use strict'
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+
 const pool = require('../database/db');
 const promisePool = pool.promise();
 
 const auth = async (req, res, next) =>{
     try{
+        console.log(req.header);
         const token = req.header('Authorization').replace('Bearer ', '');
-        const decoded = jwt.verify(token, 'thisismynewcourse');
-        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token});
-        if(!user){
-            throw new Error();
+        console.log(token);
+        const decoded = jwt.verify(token, 'webproject');
+        console.log(decoded);
+        const user_email = decoded.user_email;
+        //first we have to get the user with user_email in decoded object.
+        const [users] = await promisePool.execute('SELECT * FROM web_user WHERE user_email = ?;', [user_email]);
+        console.log(users);
+        if(users.length == 0){
+            throw Error('no user exist with this email');
         }
-        req.user = user;
-        req.token = token;
+        const user_id = users[0].user_id;
+        const [tokenRows] = await promisePool.execute('SELECT * FROM web_token where token_user_fk = ? && token_content = ?;', [user_id, token]);
+        console.log(tokenRows);
+        if(tokenRows.length == 0){
+            throw Error('token not present in database with this user_id');
+        }
+
+        //attaching user found through token to req.user
+        req.user = users[0];
+
         next();
 
     } catch(e){
-        res.status(401).send({error: 'Please authenticate.'});
+        console.log(e.message);
+        res.status(401).send({error: 'Please authenticate'});
     }
 };
 
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,10 +5,9 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 let jwt = require('jsonwebtoken');
 const userController = require('../controllers/userController');
+const auth = require('../middleware/auth.js');
 const pool = require('../database/db');
 const promisePool = pool.promise();
-
-
 
 router.get('/', async (req, res) => {
     try {
@@ -21,9 +20,9 @@ router.get('/', async (req, res) => {
       }   
 });
   
-router.get('/:id', (req, res) => {
-    res.send(`request received for user with id: ${req.params.id}`);
-});
+// router.get('/:id', (req, res) => {
+//     res.send(`request received for user with id: ${req.params.id}`);
+// });
   
  router.post('/register', async (req, res) => {
      console.log(req.body);
@@ -47,7 +46,7 @@ router.get('/:id', (req, res) => {
                 console.log(userArray[0].user_id);
                 
                 //creating json token for user who is signing up.
-                const token = jwt.sign({user_email: userArray[0].user_email}, 'web project', {expiresIn: '24h'});
+                const token = jwt.sign({user_email: userArray[0].user_email}, 'webproject', {expiresIn: '24h'});
                 console.log(token);
                 const [tokenRows] = await promisePool.execute('INSERT INTO web_token (token_user_fk, token_content) VALUES (?, ?);', [userArray[0].user_id, token]);
                 console.log(tokenRows[0]);
@@ -60,16 +59,6 @@ router.get('/:id', (req, res) => {
                 res.send({error: e.message});
                 //return {error: 'error in database query'};
             }
-
-      //res.send('post request received');
-            // try{
-            //     const [rows] = await promisePool.execute('INSERT INTO  web_user (user_name, user_email, user_address ,user_password) VALUES (?, ?, ?, ?);', params);
-            //     //const [rows] = await promisePool.execute('INSERT INTO  wop_user (name, email, password) VALUES (?, ?, ?);', params);
-            //     return rows;
-            // }catch(e){
-            //     console.log('error', e.message);
-            //     return {error: 'error in database query'};
-            // }
  });
 
  router.post('/login', async (req, res) => {
@@ -78,38 +67,89 @@ router.get('/:id', (req, res) => {
     const hash = bcrypt.hashSync(req.body.password, salt);
     try{
         const [userArray] = await promisePool.execute('SELECT * FROM web_user WHERE user_email = ?;', [req.body.email]);
-        
+        console.log(userArray);
+        console.log(userArray[0]);
+        if(userArray.length === 0){
+            throw Error('wrong username or pasword');
+        }
 
+        if(!bcrypt.compareSync(req.body.password, userArray[0].user_password)){
+            throw Error('wrong username or password');
+        };
 
-        bcrypt.compareSync(req.body.password, '$2a$12$Be6gaCpviS3r4oWx1kFYuuWand370CZftnJhQIF6Pj3Gl85dD.Tu.')
-        const user = await User.findByCredentials(req.body.email, req.body.password);
-        
-        
-        const token = await user.generateAuthToken();
-        res.send({user, token});
+        const token = generateAuthToken(userArray[0].email);
+        console.log(token);
+        const [tokenRows] = await promisePool.execute('INSERT INTO web_token (token_user_fk, token_content) VALUES (?, ?);', [userArray[0].user_id, token]);
+        console.log(tokenRows[0]);
+        res.send('post request received at /login');
+
     }catch(e){
-        res.status(400).send();
+        console.log(e.message);
+        res.status(400).send({error: e.message});
     };
-
-    // //try{
-    //    if(req.body.email  !== 'daisy@metropolia.fi' || !bcrypt.compareSync(req.body.password, '$2a$12$Be6gaCpviS3r4oWx1kFYuuWand370CZftnJhQIF6Pj3Gl85dD.Tu.')){
-    //        console.log('login', 'wrong username or password');
-    //        return res.send('login failed');
-    //    }
-    //    res.send('login successful');
-    // //}
 });  
-  
- //router.put('/', (req, res) => {
- //   res.send('put request reached at /user');
- // });
-  
- //router.delete('/', (req, res) => {
- //   res.send('delete request reached at /user');
- // });
 
+router.get('/me', auth, async (req, res) => {
+    res.send (req.user);
+});
+
+router.patch('/me', auth,  async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['user_name', 'user_address', 'user_password'];
+
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update);
+    });
+
+    try{
+
+        if(!isValidOperation){
+            throw Error('Invalid updates!');
+        }
+
+        const user_id = req.user.user_id;
+        console.log(user_id);
+
+        if(req.body.user_name){
+            const[userRows]= await promisePool.execute('UPDATE web_user SET user_name = ? WHERE user_id = ?;', [req.body.user_name, user_id]);
+        }
+
+        if(req.body.user_address){
+            const[userRows]= await promisePool.execute('UPDATE web_user SET user_address = ? WHERE user_id = ?;', [req.body.user_address, user_id]);
+        }
+
+        if(req.body.user_password){
+            const salt = bcrypt.genSaltSync(12);
+            const hash = bcrypt.hashSync(req.body.user_password, salt);
+            const[userRows]= await promisePool.execute('UPDATE web_user SET user_password = ? WHERE user_id = ?;', [hash, user_id]);
+        }
+
+        const[userRows] = await promisePool.execute('SELECT * FROM web_user WHERE user_id = ?;', [user_id]);
+        console.log(userRows[0]);
+        res.send(userRows[0]);
+    }catch(e){
+        res.status(400).send({error: e.message});
+    }
+});
+
+router.delete('/me', auth, async(req, res) => {
+    try{
+    console.log(req.user);
+    //Must also delete all cars associated with this user from web_car.
+    //to be implemented yet after car routes have been created. 
+    //then we have to delete saved tokens from web_token for the user.
+    const [tokenRows] = await promisePool.execute('DELETE FROM web_token WHERE token_user_fk = ?;', [req.user.user_id]);
+    //then finally we remove user from web_user
+    const [userRows] = await promisePool.execute('DELETE FROM web_user WHERE user_id = ?;', [req.user.user_id]);
+    res.send(req.user);
+    }catch(e){
+        res.status(500).send();
+    }
+
+});
+  
  const generateAuthToken = (user_email) => {
-    const token = jwt.sign({user_email}, 'web project', {expiresIn: '24h'});
+    const token = jwt.sign({user_email}, 'webproject', {expiresIn: '24h'});
     return token;
  }
 
